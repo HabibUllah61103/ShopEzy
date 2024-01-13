dedc6457dc9bf323cc83989b152597cb61482104
unknown <hu61103@gmail.com>
2024-01-13 19:32:19 +0500

    resolving merge conflicts

--- ShopEzy/ShopEzy_App/views.py
+++ ShopEzy/ShopEzy_App/views.py
@@ -3,8 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-<<<<<<< HEAD
     return render(request, 'index.html')
-=======
-    return render(request, "index.html")
->>>>>>> aa48f0fca8d0003eb3183128b56875c49b6c963a
