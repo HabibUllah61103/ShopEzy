483c17ad6d65d47a66b977907d5e214d6ebd4314
unknown <hu61103@gmail.com>
2024-01-13 18:45:41 +0500

    Configured static files and templates

--- ShopEzy/ShopEzy_App/views.py
+++ ShopEzy/ShopEzy_App/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-from models import customers
+# from models import customers
 # Create your views here.
 
+def index(request):
+    return render(request, "index.html")
