00770aa600435a344ac804a2bf00399df9f04e6f
unknown <hu61103@gmail.com>
2024-01-16 18:47:49 +0500

    Added Signin page with validations

--- ShopEzy/ShopEzy_App/forms.py
+++ ShopEzy/ShopEzy_App/forms.py
@@ -3,33 +3,30 @@ from .models import Customers
 from django.core.exceptions import ValidationError
 
 class SignupForm(forms.Form): 
-    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    re_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat your password'}))
+    cname = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
+    cemail = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
+    cpassword = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    cre_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat your password'}))
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email').lower()
-        new = Customers.objects.filter(cemail=email)
+    def clean_cemail(self):
+        cemail = self.cleaned_data.get('cemail').lower()
+        new = Customers.objects.filter(cemail=cemail)
         if new.exists():
             raise ValidationError("Email Already Exists")
-        return email
+        return cemail
 
-    def clean_password(self):
-        password = self.cleaned_data.get('password')
-        if len(password) < 8:
+    def clean_cpassword(self):
+        cpassword = self.cleaned_data.get('cpassword')
+        cre_password = self.cleaned_data.get('cre_password')
+
+        if len(cpassword) < 8:
             raise ValidationError("Password must be at least 8 characters long")
-        return password
 
-    def clean_re_password(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        re_password = cleaned_data.get('re_password')
-
-        if password != re_password:
+        if cpassword != cre_password:
             raise ValidationError("Passwords do not match")
-
+        return cpassword
+    
     def save(self):
-        customer = Customers.objects.create(cemail=self.cleaned_data['email'], cpassword=self.cleaned_data['password'], cname=self.cleaned_data['name'])
+        customer = Customers.objects.create(cemail=self.cleaned_data['cemail'], cpassword=self.cleaned_data['cpassword'], cname=self.cleaned_data['cname'])
         return customer
     
