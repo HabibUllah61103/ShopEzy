f23d50bf6cd9705f05a9d0b5f72156ede4fd9fbc
unknown <hu61103@gmail.com>
2024-01-16 22:51:30 +0500

    Configured top rated products in product window

--- ShopEzy/ShopEzy_App/views.py
+++ ShopEzy/ShopEzy_App/views.py
@@ -54,7 +54,6 @@ def customer_signin(request):
             user = authenticate(request, cemail=cemail, cpassword=cpassword)
             if user is not None:
                 login(request, user)
-                print(request.user.custid)
                 return redirect('product_view')           
             else:
                 return render(request, 'customer_signin.html', {'form': form})
@@ -63,32 +62,45 @@ def customer_signin(request):
     return render(request, 'customer_signin.html', {'form': form})
 
 def customer_profile(request):
+    return render(request, 'customer_profile.html', context)
+
+
+def product_view(request):
     electronics = Electronics.objects.all()
     garments = Garments.objects.all()
     groceries = Groceries.objects.all()
+    products = Products.objects.all()
+    eletronic_prod_id = {}
+    garment_prod_id = {}
+    grocery_prod_id = {}
+        
+    electronic_ids = [electronic.prodid.prodid for electronic in electronics]
+    garment_ids = [garment.prodid.prodid for garment in garments]
+    grocery_ids = [grocery.prodid.prodid for grocery in groceries]
 
-    electronic_ids = [(electronic.prodid.prodid, electronic.rating.rating) for electronic in electronics]
-    garment_ids = [(garment.prodid.prodid, garment.rating.rating) for garment in garments]
-    grocery_ids = [(grocery.prodid.prodid, grocery.rating.rating) for grocery in groceries]
+    product_id_rating = [(product.prodid, product.rating) for product in products]
+    for product_id, prodcut_rating in product_id_rating:
+        if product_id in electronic_ids:
+            eletronic_prod_id[product_id] = float(prodcut_rating)
+        elif product_id in garment_ids:
+            garment_prod_id[product_id] = float(prodcut_rating)
+        elif product_id in grocery_ids:
+            grocery_prod_id[product_id] = float(prodcut_rating)
 
-    sorted_electronic_ids = sorted(electronic_ids, key=lambda x: x[1])
-    sorted_garment_ids = sorted(garment_ids, key=lambda x: x[1])
-    sorted_grocery_ids = sorted(grocery_ids, key=lambda x: x[1])
+    sorted_electronic_prod_id = dict(sorted(eletronic_prod_id.items(), key=lambda item: item[1], reverse=True))
+    sorted_garment_prod_id = dict(sorted(garment_prod_id.items(), key=lambda item: item[1], reverse=True))
+    sorted_grocery_prod_id = dict(sorted(grocery_prod_id.items(), key=lambda item: item[1], reverse=True))
 
-    electronics_prod =[Products.objects.get(prodid=str(id)) for id in sorted_electronic_ids]
-    garments_prod =[Products.objects.get(prodid=str(id)) for id in sorted_garment_ids]
-    groceries_prod =[Products.objects.get(prodid=str(id)) for id in sorted_grocery_ids]
+    electronics_prod = [Products.objects.get(prodid=str(id)) for id in sorted_electronic_prod_id.keys()]
+    garments_prod = [Products.objects.get(prodid=str(id)) for id in sorted_garment_prod_id.keys()]
+    groceries_prod = [Products.objects.get(prodid=str(id)) for id in sorted_grocery_prod_id.keys()]
 
     context = {
         'electronics': electronics_prod,
         'garments': garments_prod,
         'groceries': groceries_prod,
     }
-
-    return render(request, 'customer_profile.html', context)
-
-def product_view(request):
-    return render(request, 'product_view.html')
+    return render(request, 'product_view.html', context)
 
 def product_detail(request):
     return render(request, 'product_detail.html')
